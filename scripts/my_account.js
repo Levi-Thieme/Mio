5d9b1e310fff363bc9c7260204b6841584c16427
Levi Thieme <thielt01@pfw.edu>
2018-10-13 00:42:43 +0000

    Added javascript for submit butttons.

---
+++ scripts/my_account.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $("#update_email_btn").click(function() {
+        $("#update_email_email_input").val("");
+        $("#update_email_password_input").val("");
+        alert("You have successfully updated your email.");
+    })
+    
+     $("#update_password_btn").click(function() {
+        alert("You have successfully updated your password.");
+    })
+    
+    
+    $("#delete_account_btn").click(function(){
+        
+    })
+    
+});
+
+
